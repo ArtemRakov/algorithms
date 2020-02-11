95e916b6f5fa90c15a002957ce386560242c51c2
Artem Rakov <scoff1k@gmail.com>
2020-02-11 11:15:25 -0500

    Update euclidean_algorithm.py

--- 02-Algebraic-algorithms/euclidean_algorithm.py
+++ 02-Algebraic-algorithms/euclidean_algorithm.py
@@ -1,9 +1,6 @@
-def solution(a, b):
-    while a !=b:
-        if a > b:
-            a = a - b
-        else:
-            b = b - a
-    return a
+def gcd(a, b):
+    if b == 0:
+       return a
+    return gcd(b, a % b)
 
-solution(125, 15)
+print(gcd(1234567890, 12))
